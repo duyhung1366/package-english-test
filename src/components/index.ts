2ba1f24adc200f9a033dcc4578c4c9c278d0f0c1
Nguyen Duy Hung <duyhungfa102@gmail.com>
2026-03-04 17:35:25 +0700

    bump version to 1.3.1 in package.json and add ExerciseSidebar export in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export { default as Spelling } from './Spelling';
 export { default as ParaGame } from './ParaGame';
 export { default as ReviewResults } from './ReviewResults';
 export { default as CircularScore } from './CircularScore';
+export { default as ExerciseSidebar } from './ExerciseSidebar';
 
 // Export types
 export type { Answer, ShowType } from './ExerciseRunner';
